82d0d7d40896351bba626c8142b62a344cfe8074
naisannovel <naisannovel@gmail.com>
2022-03-02 19:54:35 +0600

    update

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 
 const app = express();
+
+app.use(cors());
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
